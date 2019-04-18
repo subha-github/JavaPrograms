5fee19e482b84f404dfd1f01cf9ae0b8a29c38cd
subha-github <subha.github@gmail.com>
2019-04-18 06:23:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Create a build scan
+
+Follow these simple steps to create a build:
+
+1. Clone this project
+2. Run `./gradlew clean build`
---
+++ src/test/java/example/ExampleTest.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testJoin() {
+        assertEquals(null, null);
+    }
+
+}
